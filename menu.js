7011fde213b4e8437c6d171861a5ce06f6bab351
icetjanith <janithsandaruwan.bse@gmail.com>
2024-07-14 18:26:50 +0530

    20

--- menu.js
+++ menu.js
@@ -1,6 +1,6 @@
 
 
-document.getElementById('cart-icon').addEventListener('click', function() {
+/*document.getElementById('cart-icon').addEventListener('click', function() {
     if (window.innerWidth < 1200) {
         window.location.href = 'cart.html';
     } else {
@@ -20,18 +20,22 @@ document.getElementById('cart-icon').addEventListener('click', function() {
             items.style.gridTemplateColumns = 'repeat(6 ,1fr)';
         }
     }
-});
+});*/
 
 window.addEventListener('resize', function() {
-    if (window.innerWidth >= 1200) {
         const cart = document.getElementById('cart');
         const header = document.getElementById('header');
         const maincontent = document.getElementById('main-content');
         const items = document.getElementById('items');
+    if (window.innerWidth <= 1380) {
         cart.style.display = 'none';
         header.style.right = '0';
         maincontent.style.marginRight = '0';
         items.style.gridTemplateColumns = 'repeat(5 ,1fr)';
+    }else{
+        cart.style.display = 'block';
+        header.style.right = '300px';
+        maincontent.style.marginRight = '300px';
     }
 });
 
